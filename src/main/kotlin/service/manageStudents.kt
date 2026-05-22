62f9354263accf487284c296a289a66bf47f413b
luscasmbs <lucasmiguelbsilva@gmail.com>
2026-05-22 14:07:42 -0300

    fix: bug de senha curta

--- src/main/kotlin/service/manageStudents.kt
+++ src/main/kotlin/service/manageStudents.kt
@@ -1,164 +1,69 @@
-package main.kotlin.service
+package service
 
-import app.Teacherm
-import data.MemoryStorage
-import data.Student
 import data.User
-import enums.StudentSituation
+import data.Student
+import data.MemoryStorage
 import enums.UserType
+import enums.StudentSituation
 import utils.EmailFeatures
 
-
-fun managestudents(){
-    println("=================================\n " +
-            "Gerenciar alunos\n" +
-            "=================================\n\n" +
-            "1 - Alterar situação do aluno\n" +
-            "2 - Vincular aluno a uma trilha\n" +
-            "3 - Cadastrar aluno\n" +
-            "4- Ver todos os alunos\n" +
-            "0 - Voltar\n")
-    var op = readLine()?.toIntOrNull()
-    when(op){
-        1 -> {
-            println("Insira o id do aluno:")
+class RegisterUser {
+    fun executeRegister(role: UserType) {
+        val label = if (role == UserType.PROFESSOR) "Professor" else "Aluno"
+        println("=================================\n " +
+                "CADASTRO\n" +
+                "=================================\n\n" +
+                "Digite seu nome:")
+        print("> ")
+        val nome = readLine()?: "Insira um nome válido"
+        var email = ""
+        val emailUtils = EmailFeatures()
+        while (true){
+            println("Digite seu email:")
             print("> ")
-            val id = readLine()?.toIntOrNull()
-
-            val studentfind = MemoryStorage.registerUser.find { it.id == id && it.role == enums.UserType.ALUNO }
-
-            if (studentfind != null) {
-                println("Aluno encontrado: ${studentfind.name}")
-                println("Situação atual: ${studentfind.student?.student}")
-
-                println("Escolha a nova situação:\n1 - ATIVO\n2 - BLOQUEADO")
-                print("> ")
-                val situationOp = readLine()
-
-                if (situationOp == "1") {
-                    studentfind.student?.student = StudentSituation.ATIVO
-                    println("Situação do aluno atualizada para ATIVO com sucesso!")
-                } else if (situationOp == "2") {
-                    studentfind.student?.student = StudentSituation.BLOQUEADO
-                    println("Situação do aluno atualizada para BLOQUEADO com sucesso!")
-                } else {
-                    println("Opção inválida.")
-                }
-            } else {
-                println("Aluno com ID $id não encontrado.")
+            val inputemail = readLine()?: "Insira um email"
+            if (emailUtils.emailfeat(inputemail)){
+                email = inputemail
+                break
             }
         }
-        2 -> {
-            println("Insira o ID do Aluno:")
+        var senha = ""
+
+        while (true){
+            println("Digite sua senha:")
             print("> ")
-            val idAluno = readLine()?.toIntOrNull()
 
-            val aluno = MemoryStorage.registerUser.find { it.id == idAluno && it.role == UserType.ALUNO }
+            val inputSenha = readLine() ?: ""
 
-            if (aluno != null) {
-                println("Aluno selecionado: ${aluno.name}")
-                println("Insira o ID da Trilha desejada:")
-                print("> ")
-                val idTrilha = readLine()?.toIntOrNull()
-
-                val trilhaExiste = MemoryStorage.trail.any { it.id == idTrilha }
-
-                if (idTrilha != null && trilhaExiste) {
-                    val jaVinculado = aluno.student?.trail?.contains(idTrilha) ?: false
-
-                    if (!jaVinculado) {
-                        aluno.student?.trail?.add(idTrilha)
-                        println("Aluno '${aluno.name}' vinculado à trilha ID $idTrilha com sucesso!")
-                    } else {
-                        println("Erro: Este aluno já está vinculado a esta trilha!")
-                    }
-                } else {
-                    println("Erro: Trilha com ID $idTrilha não encontrada no sistema.")
-                }
+            if (inputSenha.length >= 4){
+                senha = inputSenha
+                break
             } else {
-                println("Erro: Aluno com ID $idAluno não encontrado.")
+                println("A senha deve ter pelo menos 4 caracteres")
             }
         }
-        3 -> {
-            println("Digite o Nome do Aluno:")
-            print("> ")
-            val nome = readLine() ?: ""
-            var email = ""
-            val emailUtils = EmailFeatures()
-            while (true){
-                println("Digite o E-mail do Aluno:")
-                print("> ")
-                val inputemail = readLine() ?: ""
-                if (emailUtils.emailfeat(inputemail)){
-                    email = inputemail
-                    break
-                }
-            }
-
-
-            println("Digite a Senha do Aluno:")
-            print("> ")
-            val senha = readLine() ?: ""
-
-            val emailDuplicado = MemoryStorage.registerUser.any { it.email.equals(email, ignoreCase = true) }
-
-            if (nome.isNotBlank() && email.isNotBlank() && senha.isNotBlank()) {
-                if (!emailDuplicado) {
-                    val novoId = (MemoryStorage.registerUser.maxOfOrNull { it.id } ?: 0) + 1
-
-                    val dadosEstudante = Student(
-                        student = StudentSituation.ATIVO,
-                        trail = mutableListOf(),
-                        courses = mutableListOf()
-                    )
-
-                    val novoUsuario = User(
-                        id = novoId,
-                        name = nome,
-                        email = email,
-                        password = senha,
-                        role = UserType.ALUNO,
-                        student = dadosEstudante
-                    )
-
-                    MemoryStorage.registerUser.add(novoUsuario)
-                    println("Aluno '$nome' cadastrado com sucesso! ID gerado: $novoId")
-                    return
-
-                } else {
-                    println("Erro: O e-mail '$email' já está cadastrado no sistema!")
-                }
-            } else {
-                println("Erro: Todos os campos são obrigatórios!")
-            }
+        var id = 1
+        while (MemoryStorage.registerUser.any() { it.id == id }) {
+            id++
         }
-        4 -> {
+        val studentInfo = if (role == UserType.ALUNO) {
+            Student(
+                trail = mutableListOf(),
+                student = StudentSituation.ATIVO,
+                courses = mutableListOf()
+            )
+        } else null
 
-            val listaAlunos = MemoryStorage.registerUser.filter { it.role == UserType.ALUNO }
+        val user = User(
+            id = id,
+            name = nome,
+            email = email,
+            password = senha,
+            role = role,
+            student = studentInfo
+        )
 
-            if (listaAlunos.isEmpty()) {
-                println("Nenhum aluno cadastrado no sistema até o momento.")
-            } else {
-                println(String.format("%-5s | %-20s | %-25s | %-12s | %-10s", "ID", "NOME", "E-MAIL", "SITUAÇÃO", "TRILHAS"))
-                println("---------------------------------------------------------------------------------")
-
-                for (aluno in listaAlunos) {
-                    val trilhasDoAluno = aluno.student?.trail?.joinToString(", ") ?: "Nenhuma"
-
-                    println(String.format("%-5d | %-20s | %-25s | %-12s | Trilha(s): [%s]",
-                        aluno.id,
-                        aluno.name,
-                        aluno.email,
-                        aluno.student?.student?.name ?: "N/A",
-                        trilhasDoAluno
-                    ))
-                }
-            }
-        }
-
-        0 -> return
+        MemoryStorage.registerUser.add(user)
+    }
 
     }
-    return
-
-}
